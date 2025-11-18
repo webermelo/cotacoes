7084aed609ed5185d622b63a8de109460ca5bb3f
webermelo <weber.melo@gmail.com>
2025-11-18 16:00:55 -0300

    Inicio Dev Web

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Cotações Mokka/Moica", page_icon="📄")
+
+st.title("Cotações Mokka/Moica – Versão Web (em construção)")
+st.write("Olá, Weber! Este é só um teste inicial da versão web em Streamlit.")
